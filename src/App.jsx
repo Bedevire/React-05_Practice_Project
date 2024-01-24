5abbf185017d4d368587369b32bddf27aed5d032
Adam Pavlik <adam.pavlik@exxeta.com>
2024-01-24 10:03:20 +0100

    Final fix, handle negative duration

--- src/App.jsx
+++ src/App.jsx
@@ -7,24 +7,27 @@ import { useState } from 'react'
 
 function App() {
 
-  const [inputs, setInputs] = useState([
+  const [userUnput, setUserInput] = useState([
     {title:"Initial Investment", initialValue:15000}, 
     {title:"Annual Investment", initialValue:1200}, 
     {title:"Return Expected", initialValue:6}, 
     {title:"Duration", initialValue:10}
   ]);
 
+  
   function onInputChangeApp(title, value){
-    let newInputs = [...inputs];
+    let newInputs = [...userUnput];
     let inputParam = newInputs.find(item => item.title == title);
     inputParam.initialValue = parseInt(value);
-    setInputs(items => newInputs);
+    setUserInput(items => newInputs);
   }
-
-  const initialInvestment = inputs.find(item => item.title == 'Initial Investment').initialValue;
-  const annualInvestment = inputs.find(item => item.title == 'Annual Investment').initialValue;
-  const expectedReturn = inputs.find(item => item.title == 'Return Expected').initialValue;
-  const duration = inputs.find(item => item.title == 'Duration').initialValue;
+  
+  const initialInvestment = userUnput.find(item => item.title == 'Initial Investment').initialValue;
+  const annualInvestment = userUnput.find(item => item.title == 'Annual Investment').initialValue;
+  const expectedReturn = userUnput.find(item => item.title == 'Return Expected').initialValue;
+  const duration = userUnput.find(item => item.title == 'Duration').initialValue;
+  
+  const inputIsValid = duration >= 1;
 
   let annualData = calculateInvestmentResults({initialInvestment, annualInvestment, expectedReturn, duration});
   console.log(annualData);
@@ -32,8 +35,9 @@ function App() {
   return (
     <>
       <Header />
-      <Controls inputs={inputs} onInputChangeApp={onInputChangeApp} />
-      <ResultTable data={annualData} />
+      <Controls inputs={userUnput} onInputChangeApp={onInputChangeApp} />
+      {inputIsValid && (<ResultTable data={annualData} />)}
+      {!inputIsValid && <p className="center">Please enter a positive duration</p>}
     </>
 
   )
